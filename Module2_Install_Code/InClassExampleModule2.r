2ee19b22a3c78a4ac46e49d49a86c2067e7a7615
Elizabeth Babcock <ebabcock@rsmas.miami.edu>
2026-08-20 14:16:57 -0400

    make file visible

---
+++ Module2_Install_Code/InClassExampleModule2.r
@@ -0,0 +1,8 @@
+library(tidyverse)
+bruv<-read.csv("bruv.csv")
+theme_set(theme_minimal())
+ggplot(bruv,aes(x=Zone,fill=Sharks))+
+  geom_bar()+
+  scale_fill_manual(values=c("darkgreen","lavender"))+
+  labs(x="",y="Number of BRUV samples",fill="Sharks present?")+
+  theme(legend.position="top")
