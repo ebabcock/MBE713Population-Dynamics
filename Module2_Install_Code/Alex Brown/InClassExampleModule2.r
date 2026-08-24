56f542113ec34cd1040a257faf7e2f8cf7db5e4b
alexbrown0380 <arb383@miami.edu>
2026-08-24 13:27:09 -0400

    move files and revise Module 2 code

--- Module2_Install_Code/Alex Brown/InClassExampleModule2.r
+++ Module2_Install_Code/Alex Brown/InClassExampleModule2.r
@@ -3,7 +3,7 @@ bruv<-read.csv("bruv.csv")
 theme_set(theme_minimal())
 ggplot(bruv,aes(x=Zone,fill=Sharks))+
   geom_bar()+
-  scale_fill_manual(values=c("darkgreen","lavender"))+
+  scale_fill_manual(values=c("blue","pink"))+
   labs(x="",y="Number of BRUV samples",fill="Sharks present?")+
   theme(legend.position="top")
 # I edited this on the website 
