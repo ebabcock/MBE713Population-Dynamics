4a6f9fd2a089f9dc181066134d2f9725fb264671
Aliviare <axe1096@miami.edu>
2026-08-24 19:23:56 -0400

    Assignment 8/25 pt.2

---
+++ Module2_Install_Code/Alivia Eggleston/InClassExampleModule2.r
@@ -0,0 +1,11 @@
+library(tidyverse)
+bruv<-read.csv("bruv.csv")
+theme_set(theme_minimal())
+ggplot(bruv,aes(x=Zone,fill=Sharks))+
+  geom_bar()+
+  scale_fill_manual(values=c("pink","lightblue"))+
+  labs(x="",y="Number of BRUV samples",fill="Sharks present?")+
+  theme(legend.position="top")
+# I edited this on the website
+# I can see your edits!
+# This is my comment for 8/25!
