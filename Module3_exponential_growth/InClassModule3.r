15a727b1433516ebe6bda8e2477d8fc0ef4f7deb
cco67 <cco67@miami.edu>
2026-09-03 13:37:56 -0400

    commiting files

--- Module3_exponential_growth/InClassModule3.r
+++ Module3_exponential_growth/InClassModule3.r
@@ -68,4 +68,58 @@ rm_per_day<-0.001 # per day
 rm_per_year<-rm_per_day*365
 rm_per_year
 
+library(tidyverse)
+humans<-read_csv("HumanPopulation.csv") %>%
+  filter(Year <1900)   
+ggplot(humans,aes(x=Year,y=Population))+
+  geom_point()
 
+ggplot(humans,aes(x=Year,y=log(Population)))+
+  geom_point()+
+  stat_smooth(method="lm")
+
+#do linear regression
+humans.lm<-lm(log(Population)~Year,data=humans)
+summary(humans.lm)
+coef(humans.lm)
+#y=intercept + slope* x
+#y=log(N)
+#x=t
+#slope=rm
+#log(N0)=intercept
+#N0=exp(intercept)
+rm<-coef(humans.lm)[2]
+rm
+N0<-exp(coef(humans.lm)[1])
+N0
+#plot
+ggplot(humans,aes(x=Year))+
+  geom_point(aes(y=Population))+
+  geom_function(fun=~N0*exp(rm*.x)) 
+#lambda
+lambda=exp(rm)
+lambda
+
+
+# Linear Regression estimation
+library(tidyverse)
+ggplot(mpg,aes(x=hwy,y=cty))+
+  geom_point()+
+  stat_smooth(method="lm")+
+  labs(x="Highway miles per gallon",y="City miles per gallon",
+       title="Fuel efficiency regression")
+cars.lm<-lm(cty~hwy,data=mpg)
+coef(cars.lm)
+
+RegressionSS<-function(par,x,y) {
+  a<-par["a"]
+  b<-par["b"]
+  y_hat<-a+b*x
+  SS<-sum((y-y_hat)^2)
+  return(SS)
+}
+fit<-optim(par=c(a=0,b=1),
+           fn=RegressionSS,
+           x=mpg$hwy,
+           y=mpg$cty)
+fit
